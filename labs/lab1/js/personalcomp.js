8a00707fdfe944a58ddfafc626dd27a898e8f62e
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2021-01-28 15:56:28 -0500

    Completed Personal Composition

---
+++ labs/lab1/js/personalcomp.js
@@ -0,0 +1,24 @@
+function setup()
+{
+    let grey = "#4f4f4f";
+    let yellow = "#e5e502";
+    let lBlue = "#9ff2e5";
+    createCanvas(800,600);
+    noStroke();
+    fill(yellow);
+    
+    rect(200, 200, 500, 200);
+
+    fill(0,0,0);
+    circle(300, 400, 50);
+    circle(600, 400, 50);
+
+    fill(grey);
+    circle(300, 400, 30);
+    circle(600, 400, 30);
+
+    fill(lBlue);
+    rect(200, 225, 100, 100);
+
+    
+}
