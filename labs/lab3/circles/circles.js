d140d470abe9cbbcbb92e8d99430f83f42ed8e88
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2021-02-11 16:39:29 -0500

    Formatting changes

--- labs/lab3/circles/circles.js
+++ labs/lab3/circles/circles.js
@@ -4,9 +4,10 @@ function setup()
     createCanvas(1000, 1000);
     noFill();
     background(150, 150, 150);
-    while(r < 500)
+
+    while(r < 500) 
     {
-        circle(500, 500, r);
+        circle(500, 500, r); 
         r += 10;
     }
 }
--- labs/lab3/fizzbuzz/fizzbuzz.js
+++ labs/lab3/fizzbuzz/fizzbuzz.js
@@ -1,32 +1,31 @@
 var ballPos = 0;
 
-function setup()
-{
+function setup() {
     let circlePos = 10;
     createCanvas(800, 600);
-    for(let i = 0; i < 25; i++)
+    for (let i = 0; i < 25; i++) 
     {
-        if(i % 3 === 0 && i % 5 === 0)
+        if (i % 3 == 0 && i % 5 == 0) 
         {
-            fill(0,0,200);
-            rect(circlePos - 10, 290, 20, 20)
+            fill(0, 0, 200);
+            rect(circlePos - 10, 290, 20, 20);
             noFill();
         }
-        else if(i % 3 == 0)
+        else if (i % 3 == 0) 
         {
             fill(200, 0, 200);
             circle(circlePos, 300, 10);
             noFill();
         }
-        else if(i % 5 == 0)
+        else if (i % 5 == 0) 
         {
-            fill(0,100,0);
-            rect(circlePos - 10, 290, 20, 20)
+            fill(0, 100, 0);
+            rect(circlePos - 10, 290, 20, 20);
             noFill();
         }
-        else
+        else 
         {
-            fill(0,0,0);
+            fill(0, 0, 0);
             circle(circlePos, 300, 10);
         }
         circlePos += 20;
